a8cc8ffe54d055502fc005c9b6f5adbae7be94e0
jjowens <jj.owens.dev@gmail.com>
2021-06-07 11:18:13 -0500

    Add layout, add button test, add material, add global css

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import React from 'react'
+import { Button } from '@material-ui/core';
+
 function HomePage() {
     return (
         <div>
             <h1>Welcome to the Homepage!</h1>
+            <Button variant="contained" color="primary">Button Test</Button>
         </div>
     )
 }
